1a664cd7a4e3058f82b601705d2696a7a2fdf358
105 | Sebastian <sebastianschapfel105@gmail.com>
2025-09-08 00:04:12 +0000

    f

--- school/src/components/universal.jsx
+++ school/src/components/universal.jsx
@@ -8,10 +8,21 @@ export default function Universal() {
     <>
     <navbar
     id="navbar">
-       
+       <div className='leftNav'>
           <Link className="nav-link" to="https://sebastian-105.com">
-            asd 
+            <img src="https://sebastian-105.com/105.png" style={{ width: '35%', height: '35%', borderRadius: '50%', marginTop:'7px'}} alt="description" />
           </Link>
+          </div>
+          <div className='rightNav'>
+          <Link className="nav-link" to="/todo">Todo</Link>
+          <Link className="nav-link" to="/assignmentTracker">Assignment Tracker</Link>
+          <Link className="nav-link" to="/schedule">Schedule</Link>
+          </div>
+          {/* <div className='rightNav'>
+          <Link className="nav-link" to="">
+           
+          </Link>
+          </div> */}
     </navbar>
     <footer>
       © Sebastian Schapfel 2025
